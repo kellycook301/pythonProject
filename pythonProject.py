303cd0d9ebebf37d5a3aa01ef951257023c01045
Kelly Cook <kellycook20878@gmail.com>
2019-02-06 13:30:58 -0600

    learning concatination in python

--- pythonProject.py
+++ pythonProject.py
@@ -1,7 +1,9 @@
-first_name = "Ada"
+first_name = input("What is your first name?  ")
+dessert = "chocolate" + " marshmallows"
 
 print("Hello, Kelly!")
-print("Hello,", first_name)
+print('Hello,', first_name)
 
 print("Kelly is learning Python")
-print(first_name, "is learning Python")
+print(first_name, 'is learning Python')
+print(dessert)
