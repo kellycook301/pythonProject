380ab03c6ff44e663cab54ae997c2874828661d0
Kelly Cook <kellycook20878@gmail.com>
2019-02-12 09:40:06 -0600

    working on bolleans in python

--- pythonProject.py
+++ pythonProject.py
@@ -2,6 +2,9 @@ first_name = input("What is your first name?  ")
 dessert = "chocolate" + " marshmallows"
 quote = "A person who never made a mistake never tried anything new"
 newQuote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW"
+subject_template = "Thanks for learning {} with us, {}!"
+subject_template.format("Javascript", "Kelly")
+print("You just bought {} {} from us, {}.".format(3, "fidget cubes", "Kelly"))
 
 print(quote.upper())
 print(newQuote.lower())
