517d8bc8ddfeb44ab50e715b0a072db02d868916
Kelly Cook <kellycook20878@gmail.com>
2019-02-18 12:47:26 -0600

    did if/else/elif statements

--- pythonProject.py
+++ pythonProject.py
@@ -1,21 +1,28 @@
-# if statement in python
+# 1. If/Else/Elif Statements In Python
+# ______________________
 first_name = input("What is your first name?  ")
 print("Hello,", first_name)
 if first_name == "Kelly":
     print(first_name, "is learning Python")
+elif first_name =="Maximiliane":
+    print(first_name, "is learning with fellow students in the community! Me too!")
+else: 
+    print("You should TOTALLY learn Python, {}!".format(first_name))
 print("Have a great day, {}!".format(first_name))
 
-dessert = "chocolate" + " marshmallows"
-quote = "A person who never made a mistake never tried anything new"
-newQuote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW"
-subject_template = "Thanks for learning {} with us, {}!"
-subject_template.format("Javascript", "Kelly")
-print("You just bought {} {} from us, {}.".format(3, "fidget cubes", "Kelly"))
+#2. Establishing Variables
+# ______________________
+# dessert = "chocolate" + " marshmallows"
+# quote = "A person who never made a mistake never tried anything new"
+# newQuote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW"
+# subject_template = "Thanks for learning {} with us, {}!"
+# subject_template.format("Javascript", "Kelly")
+# print("You just bought {} {} from us, {}.".format(3, "fidget cubes", "Kelly"))
 
-print(quote.upper())
-print(newQuote.lower())
+# print(quote.upper())
+# print(newQuote.lower())
 
-print(dessert)
+# print(dessert)
 
 # establishing that "fruit" equals "apples"
 fruit = "apples"
