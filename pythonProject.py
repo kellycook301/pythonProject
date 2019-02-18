3d5851727aa3dc0a3fc697ce8d2dad04de306929
Kelly Cook <kellycook20878@gmail.com>
2019-02-18 12:15:45 -0600

    learning if statements in python

--- pythonProject.py
+++ pythonProject.py
@@ -1,4 +1,10 @@
+# if statement in python
 first_name = input("What is your first name?  ")
+print("Hello,", first_name)
+if first_name == "Kelly":
+    print(first_name, "is learning Python")
+print("Have a great day, {}!".format(first_name))
+
 dessert = "chocolate" + " marshmallows"
 quote = "A person who never made a mistake never tried anything new"
 newQuote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW"
@@ -9,9 +15,11 @@ print("You just bought {} {} from us, {}.".format(3, "fidget cubes", "Kelly"))
 print(quote.upper())
 print(newQuote.lower())
 
-print("Hello, Kelly!")
-print('Hello,', first_name)
-
-print("Kelly is learning Python")
-print(first_name, 'is learning Python')
 print(dessert)
+
+# establishing that "fruit" equals "apples"
+fruit = "apples"
+# checking to see if "fruit" equals "apples"
+fruit == "apples"
+# checking to see if "fruit" equals
+fruit == "oranges"
