51e7d061cd92deeadee70e394f2ef7b53185a394
Kelly Cook <kellycook20878@gmail.com>
2019-02-26 13:27:12 -0600

    implemented trys into function for exceptions

--- checkPlease.py
+++ checkPlease.py
@@ -7,14 +7,22 @@ import math
 
 # defining function named "split_check"
 def split_check(total, number_of_people):
+    if number_of_people <= 1:
+        raise ValueError("More than one person is required to split a check")
     return math.ceil(total / number_of_people)
 
-total_due = float(input ("What is the total?  "))
-number_of_people = int(input("How many people?  "))
-amount_due = split_check(total_due, number_of_people)
-
-
-print("Each person owes ${}. Pay up!".format(amount_due))
+# might throw a SyntaxError
+try: 
+    total_due = float(input ("What is the total?  "))
+    number_of_people = int(input("How many people?  "))
+    amount_due = split_check(total_due, number_of_people)
+except SyntaxError:
+    print("Oh no! That's not a valid value. Try again...")
+except ValueError as err:
+    print("Oh no! That's not a valid value. Try again...")
+    print("({})".format(err))
+else:
+    print("Each person owes ${}. Pay up!".format(amount_due))
 
 # creating a function for something that calculates the square of
 # a number passed in
